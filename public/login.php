28f4cbddd0f0e466e19b839059cd3ec9b62bd1a6
Talal <mail@ta1al.com>
2024-11-23 10:25:32 +0500

    feat(login): store username in session upon successful login

--- public/login.php
+++ public/login.php
@@ -12,6 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   if ($user && password_verify($password, $user['password'])) {
     session_start();
     $_SESSION['user_id'] = $user['id'];
+    $_SESSION['username'] = $user['username'];
     echo "Login successful!";
     header("Location: /");
     exit;
