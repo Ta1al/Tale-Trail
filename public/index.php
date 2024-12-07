7e54bd9a3d9c6dcadd031ca23c097af8f246d46d
Talal <mail@ta1al.com>
2024-12-07 11:11:57 +0500

    refactor(index.php): enhance routing logic for user authentication and session management

--- public/index.php
+++ public/index.php
@@ -6,12 +6,47 @@ use App\Controllers\UserController;
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
-if ($uri === '/register' && $method === 'POST') {
-  $controller = new UserController();
-  $controller->register();
-} elseif ($uri === '/login' && $method === 'POST') {
-  $controller = new UserController();
-  $controller->login();
-} else {
-  echo "404 - Page not found.";
+$userController = new UserController();
+switch ($method) {
+  case 'GET':
+    switch ($uri) {
+      case '/':
+        require_once '../src/views/home.php';
+        break;
+
+      case '/register':
+        require_once '../src/views/auth/register.php';
+        break;
+
+      case '/login':
+        if (isset($_SESSION['user_id'])) {
+          header("Location: /");
+          exit;
+        }
+        require_once '../src/views/auth/login.php';
+        break;
+
+      case '/logout':
+        $userController->logout();
+        break;
+      default:
+        http_response_code(404);
+        echo "Page not found.";
+        break;
+    }
+
+    if ($uri === '/logout')
+      $userController->logout();
+
+    break;
+  case 'POST':
+    if ($uri === '/register')
+      $userController->register();
+
+    if ($uri === '/login')
+      $userController->login();
+    break;
+  default:
+    echo "Method not allowed.";
+    break;
 }
