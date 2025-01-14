19eb6357fe350674143dc7df58dbe1550787b318
Talal <mail@ta1al.com>
2025-01-14 17:58:31 +0500

    feat(auth): add login page styling and structure

--- src/views/auth/login.php
+++ src/views/auth/login.php
@@ -3,18 +3,21 @@
 
 <head>
   <title>Login</title>
+  <link rel="stylesheet" type="text/css" href="/css/login.css">
 </head>
 
 <body>
-  <h1>Login</h1>
-  <form method="POST" action="/login">
-    <label>Username:</label>
-    <input type="text" name="username" required><br>
-    <label>Password:</label>
-    <input type="password" name="password" required><br>
-    <button type="submit">Login</button>
-  </form>
-  <p>Don't have an account? Register <a href="/register">here</a>.</p>
+  <div class="container">
+    <h1>Login</h1>
+    <form method="POST" action="/login">
+      <label>Username:</label>
+      <input type="text" name="username" required><br>
+      <label>Password:</label>
+      <input type="password" name="password" required><br>
+      <button type="submit" class="button">Login</button>
+    </form>
+    <p>Don't have an account? Register <a href="/register">here</a>.</p>
+  </div>
 </body>
 
 </html>
