f535297de6fa4b3c7e182264b96dde28e38abd55
Talal <mail@ta1al.com>
2025-01-14 17:27:08 +0500

    feat(home): add home page styling and update layout

--- src/views/home.php
+++ src/views/home.php
@@ -4,20 +4,29 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Welcome</title>
+  <title>Welcome to Tale Trail</title>
+  <link rel="stylesheet" href="/css/home.css">
 </head>
 
 <body>
-  Hello World.
-  <?php
-  // if the user is logged in, display a welcome message
-  if (isset($_SESSION['username'])) {
-    echo 'Welcome, ' . $_SESSION['username'];
-    echo '<br><a href="/logout">Logout</a>';
-  } else {
-    echo 'Login by clicking <a href="/login">here</a>';
-  }
-  ?>
+  <div class="container">
+    <h1>Welcome to Tale Trail</h1>
+    <p>Your adventure starts here.</p>
+    <div class="links">
+      <?php
+      // if the user is logged in, display a welcome message and relevant links
+      if (isset($_SESSION['username'])) {
+        echo '<p>Welcome, ' . $_SESSION['username'] . '!</p>';
+        echo '<a href="/story/view" class="button">View Stories</a>';
+        echo '<a href="/story/create" class="button">Create Story</a>';
+        echo '<a href="/logout" class="button">Logout</a>';
+      } else {
+        echo '<a href="/login" class="button">Login</a>';
+        echo '<a href="/register" class="button">Register</a>';
+      }
+      ?>
+    </div>
+  </div>
 </body>
 
 </html>
